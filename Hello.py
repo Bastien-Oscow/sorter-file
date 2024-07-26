05de908d33fff95e152a94c96da829aa43f55ad6
Bastien-Oscow <131648788+Bastien-Oscow@users.noreply.github.com>
2024-07-26 21:26:52 +0200

    first-sorter

--- Hello.py
+++ Hello.py
@@ -2,6 +2,11 @@ import os   # based | telechargée avec python
 fileList = os.listdir("C:\\Users\\School.LAPTOP-GN5E6HKO\\Downloads\\") #store all file 
 for i in fileList :
     #print (i)
-    test = i.split(".")
-    print (test)
+    
+    scrap = i.split(".")    #split
+    print (scrap)
+    if len(scrap) >= 2 : 
+        print(scrap[-1])    #take last digit | prend le dernier element de la liste
+    else :
+        print("folder",i)
 
